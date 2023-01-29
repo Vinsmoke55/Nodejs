bf52c429a841aaa08feb9af1b133963652b31e1c
VINANK <neupaneayush3@gmail.com>
2023-01-29 14:57:11 +0545

    storing data with json

---
+++ challenges/json/1json.js
@@ -0,0 +1,19 @@
+//requiring the fs module
+const fs=require('fs');
+
+var dataBuffer=fs.readFileSync('data.json'); //reading the file data.json which come in the form of bit and byte and storing it to databuffer
+var dataString=dataBuffer.toString();	//to read the data in data.json we have to convert buffer to string
+
+var dataObject=JSON.parse(dataString);	//parsing the dataString into a object
+
+console.log(dataObject.name);	//now we can get value from the object
+
+//updating the data in dataObject 
+dataObject.name="hari";
+dataObject.age=21;
+
+console.log(dataObject);
+
+var updatedDataString=JSON.stringify(dataObject);	//stringiying the object
+
+fs.writeFileSync('data.json',updatedDataString);	//writing the updated data in the data.json file
