340b20a3bd4e21e5d43e454feb6b5ce880f9ca44
VINANK <neupaneayush3@gmail.com>
2023-02-02 13:38:14 +0545

    debugging Node js

---
+++ challenges/debugger/debug.js
@@ -0,0 +1,13 @@
+//debugger in node js
+
+var a=1;
+var b=2;
+
+console.log(a+b);
+
+/*this line can be used to debug 
+we can debug by typing node inspect debug.js and
+visiting the crome://inspect in the chrome browser*/
+debugger			
+
+console(a-b);
