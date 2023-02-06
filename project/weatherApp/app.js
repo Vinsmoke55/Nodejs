a5ff2b8d8e143940e7c48c7faa1c7b67fe64bbe9
VINANK <neupaneayush3@gmail.com>
2023-02-06 12:56:05 +0545

    updating with object shorthand and destructuring

--- project/weatherApp/app.js
+++ project/weatherApp/app.js
@@ -1,11 +1,23 @@
 const geoCode=require('./utils/geocode.js')
 const weather=require('./utils/weather.js')
 
-geoCode('kathmandu',(error,data)=>{
-	weather(data.latitude,data.longitude,(error,data)=>{
-	console.log(data)
+//process.agrv aceepts location via the command line argument
+var address=process.argv[2];
+if(!address){
+	console.log('please provide the address')
+}
+else{
+	geoCode(address,(error,{latitude,longitude})=>{	//data is an object so destrcturing it 
+	if(error){
+		return console.log(error)
+	}
+	weather(latitude,longitude,(error,weatherdata)=>{
+	console.log(weatherdata)
 })
-})	
+}) 
+}
+	
+
 
 	
 
--- project/weatherApp/utils/geocode.js
+++ project/weatherApp/utils/geocode.js
@@ -3,17 +3,17 @@ const request=require('request')
 var geoCode=(city,callback)=>{
 	const geocodeURL='https://api.api-ninjas.com/v1/geocoding?city='+city;
 
-	request({url:geocodeURL,headers: {'X-Api-Key': '5qaxvKD2gEZ53oGe/AuzsQ==Z27MXTAXLCECGFz7'},json:true},(error,response)=>{
+	request({url:geocodeURL,headers: {'X-Api-Key': '5qaxvKD2gEZ53oGe/AuzsQ==Z27MXTAXLCECGFz7'},json:true},(error,{body})=>{
 		if(error){						
 			callback("there is eror in the program",undefined)
 		}
-		else if(response.body.error){	
+		else if(body.error){	
 			callback("data error",undefined)
 		}
 		else{
 			const data={
-				latitude:response.body[0].latitude,
-				longitude:response.body[0].longitude
+				latitude:body[0].latitude,
+				longitude:body[0].longitude
 			}
 			;
 			callback(undefined,data)
