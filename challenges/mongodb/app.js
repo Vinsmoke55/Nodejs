44eca8b1c3fedebbc0a76cdf08aff321ebde838b
VINANK <neupaneayush3@gmail.com>
2023-02-14 15:31:05 +0545

    creating a mongoose model

--- challenges/mongodb/app.js
+++ challenges/mongodb/app.js
@@ -5,25 +5,44 @@ const dbURL='mongodb+srv://ayush:helloworld55@cluster6.nvxaegs.mongodb.net/?retr
 
 mongoose.connect(dbURL)
 
-const user=mongoose.model('User',{
-	name:{
+// const user=mongoose.model('User',{
+// 	name:{
+// 		type:'string'
+// 	},
+// 	age:{
+// 		type:Number
+// 	}
+// })
+
+// const me=new user({
+// 	name:'ayush',
+// 	age:20
+
+// })
+
+// me.save().then(()=>{
+// 	console.log('saved')
+// }).catch(()=>{
+// 	console.log('failed')
+// })
+
+
+const tasks=mongoose.model('Task',{
+	description:{
 		type:'string'
 	},
-	age:{
-		type:Number
+	completed:{
+		type:Boolean
 	}
 })
 
-const me=new user({
-	name:'ayush',
-	age:20
-
+const newTask=new tasks({
+	description:"do your homework",
+	completed:false
 })
 
-me.save().then(()=>{
-	console.log('saved')
+newTask.save().then(()=>{
+	console.log(newTask)
 }).catch(()=>{
 	console.log('failed')
 })
-
-
