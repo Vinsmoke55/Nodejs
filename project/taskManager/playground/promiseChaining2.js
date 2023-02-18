b1f6c7a976907a0822fba3dfe1d8b3220e1395a1
VINANK <neupaneayush3@gmail.com>
2023-02-18 17:59:45 +0545

    promise chaining challenge

---
+++ project/taskManager/playground/promiseChaining2.js
@@ -0,0 +1,11 @@
+require('../src/db/mongoose.js')
+const Task=require('../src/models/task.js')
+
+Task.findByIdAndRemove('63eb57e4db4c4b985fd9222f').then((user)=>{
+	console.log(user)
+	return Task.countDocuments({completed:false})
+}).then((result)=>{
+	console.log(result)
+}).catch((e)=>{
+	console.log(e)
+})
