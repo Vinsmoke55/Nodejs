604f6e0ff643e02e22a319a6420a5b90292189bb
VINANK <neupaneayush3@gmail.com>
2023-02-19 10:59:02 +0545

    updating playground with async-await

--- project/taskManager/playground/promiseChaining.js
+++ project/taskManager/playground/promiseChaining.js
@@ -1,10 +1,24 @@
 require('../src/db/mongoose.js')
 const User=require('../src/models/user.js')
 
-User.findByIdAndUpdate('63eb6069200d358ee3687762',{age:50}).then((user)=>{
-	console.log(user)
-	return User.countDocuments({age:50})
-}).then((result)=>{
+
+
+// User.findByIdAndUpdate('63eb6069200d358ee3687762',{age:50}).then((user)=>{
+// 	console.log(user)
+// 	return User.countDocuments({age:50})
+// }).then((result)=>{
+// 	console.log(result)
+// }).catch((e)=>{
+// 	console.log(e)
+// })
+
+const updateAndCount=async(id,age)=>{
+	const user=await User.findByIdAndUpdate(id,{age:age})
+	const count=await User.countDocuments({age:age})
+	return count
+}
+
+updateAndCount('63eb6069200d358ee3687762',20).then((result)=>{
 	console.log(result)
 }).catch((e)=>{
 	console.log(e)
