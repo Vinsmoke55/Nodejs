15835dec69990498d9f46f2832f4f091f5154496
VINANK <neupaneayush3@gmail.com>
2023-02-23 17:54:00 +0545

    express middleware

--- project/taskManager/src/index.js
+++ project/taskManager/src/index.js
@@ -5,6 +5,19 @@ const taskRouter=require('./router/taskroute.js')
 
 const app=express()
 
+// app.use((req,res,next)=>{		//express middleware
+// 	if(req.method==='GET'){
+// 		res.send("GET request are currently disabled")
+// 	}
+// 	else{
+// 		next()
+// 	}
+// })
+
+app.use((req,res,next)=>{
+	res.status(503).send('the app is currently under maintainance')
+})
+
 app.use(express.json())		//this line parse the incoming json 
 app.use(userRouter)			//using user router
 app.use(taskRouter)			//using task router
