8f353d73de2ddae0901c9c53b12c6613570c9577
VINANK <neupaneayush3@gmail.com>
2023-03-11 13:56:45 +0545

    getting started with socket.io

--- project/chatApp/src/index.js
+++ project/chatApp/src/index.js
@@ -1,16 +1,21 @@
+//requiring the modules
 const path=require('path')
+const http=require('http')
 const express=require('express')
+const socketio=require('socket.io')
 
-const app=express()
+const app=express()		//setting the express
+const server=http.createServer(app)	//creating a server
+const io=socketio(server)	//passing server to socketio
 
 const pathToPublic=path.join(__dirname,'../public')
 
-app.use(express.static(pathToPublic))
+app.use(express.static(pathToPublic))	//passing the static files
 
-// app.get('',(req,res)=>{
-// 	res.send()
-// })
+io.on('connection',()=>{
+	console.log("new connection established")
+})
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{	//listening to the server
 	console.log('app running at port 3000')
 })
